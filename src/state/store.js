df49d8fb899a4e3fbc82c8f3226c48178854ecaa
Panos Anastasopoulos <anastaso.pan@gmail.com>
2020-08-10 17:00:26 +0300

    adds key tictactoeRedux and changes store initial state

--- src/state/store.js
+++ src/state/store.js
@@ -6,11 +6,11 @@ const initialState = {
   players: { player1: '', player2: '' },
   ticTacToe: { winner: null, squares: Array(9).fill(null), xIsNext: true },
 };
-const retreivedState = JSON.parse(localStorage.getItem('tictactoe'))||initialState;
+const retreivedState = JSON.parse(localStorage.getItem('tictactoeRedux'))||initialState;
 
 const store = createStore(
   rootReducer,
-  areKeysEqual(initialState, retreivedState) ? retreivedState : initialState,
+  retreivedState ,
   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__(
     applyMiddleware(persistStateMiddleware)
   )
