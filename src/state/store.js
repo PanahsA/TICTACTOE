dd01f3264918a023b4ad312d3e15324ca4509aef
Panos Anastasopoulos <anastaso.pan@gmail.com>
2020-08-10 15:26:38 +0300

    adds areKeysEqual, not working

--- src/state/store.js
+++ src/state/store.js
@@ -2,11 +2,23 @@ import { createStore, applyMiddleware } from 'redux';
 import { persistStateMiddleware } from '../utils';
 import rootReducer from './reducers';
 
+const initialState = {
+  players: { player1: '', player2: '' },
+  ticTacToe: { winner: null, squares: Array(9).fill(null), xIsNext: true },
+};
 const retreivedState = JSON.parse(localStorage.getItem('tictactoe'));
+
+function areKeysEqual() {
+  var aKeys = Object.keys(initialState).sort();
+  var bKeys = Object.keys(retreivedState).sort();
+  return aKeys.join('') === bKeys.join('');
+}
+
+areKeysEqual();
 
 const store = createStore(
   rootReducer,
-  retreivedState,
+  areKeysEqual() ? retreivedState : initialState,
   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__(
     applyMiddleware(persistStateMiddleware)
   )
