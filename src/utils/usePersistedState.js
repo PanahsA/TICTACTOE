358c764ee28ce497673342b3ee66c0f34c4cf5a3
Panos Anastasopoulos <anastaso.pan@gmail.com>
2020-08-10 17:07:34 +0300

    deletes areKeysEqual()

--- src/utils/usePersistedState.js
+++ src/utils/usePersistedState.js
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react';
-import areKeysEqual from './areKeysEqual';
 
 const usePersistedState = (key, initialState) => {
   const [persistedState, setPersistedState] = useState(() => {
