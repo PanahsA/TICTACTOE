5e579c192fb2a86ff9b2235cd2c38794faabd414
Panos Anastasopoulos <anastaso.pan@gmail.com>
2020-08-10 17:01:54 +0300

    fixes on key

---
+++ src/state/store.js
@@ -0,0 +1,19 @@
+import { createStore, applyMiddleware } from 'redux';
+import { persistStateMiddleware, areKeysEqual } from '../utils';
+import rootReducer from './reducers';
+
+const initialState = {
+  players: { player1: '', player2: '' },
+  ticTacToe: { winner: null, squares: Array(9).fill(null), xIsNext: true },
+};
+const retreivedState = JSON.parse(localStorage.getItem('tictactoeReact'))||initialState;
+
+const store = createStore(
+  rootReducer,
+  retreivedState ,
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__(
+    applyMiddleware(persistStateMiddleware)
+  )
+);
+
+export default store;
---
+++ src/utils/persistStateMiddleware.js
@@ -0,0 +1,9 @@
+const persistStateMiddleware = (store) => (next) => (action) => {
+  const result = next(action);
+  console.log(action, 'dispatched');
+  const nextState = store.getState();
+  localStorage.setItem('tictactoeReact', JSON.stringify(nextState));
+  return next(result);
+};
+
+export default persistStateMiddleware;
