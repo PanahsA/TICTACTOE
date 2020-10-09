b793699705018a2a1c26f81106bf0d7d0f115094
Panos Anastasopoulos <anastaso.pan@gmail.com>
2020-10-09 13:29:15 +0300

    removes ReduxDevTools for compatibility

--- src/state/store.js
+++ src/state/store.js
@@ -6,14 +6,13 @@ const initialState = {
   players: { player1: '', player2: '' },
   ticTacToe: { winner: null, squares: Array(9).fill(null), xIsNext: true },
 };
-const retreivedState = JSON.parse(localStorage.getItem('tictactoeRedux'))||initialState;
+const retreivedState =
+  JSON.parse(localStorage.getItem('tictactoeRedux')) || initialState;
 
 const store = createStore(
   rootReducer,
-  retreivedState ,
-  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__(
-    applyMiddleware(persistStateMiddleware)
-  )
+  retreivedState,
+  applyMiddleware(persistStateMiddleware)
 );
 
 export default store;
